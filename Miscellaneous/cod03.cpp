8f86d75f6bd80d7081fae028d02938724bd536da
Claudia2000 <42683698+Claudia2000@users.noreply.github.com>
2019-05-03 21:10:57 +0000

    Add files via upload

---
+++ Miscellaneous/cod03.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<stdlib.h>
+#include<stdexcept>
+using namespace std;
+
+int main(){
+	int a, b, c;
+	cout<<"Ingrese tres numeros"<<endl;
+	cin>>a>>b>>c;
+	if(a==b&&a==c)
+		cout<<"Son iguales"<<endl;
+}
